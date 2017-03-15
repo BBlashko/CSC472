a6d751b0b489822a31cbe21122edae6a67ebdd0a
Brett Blashko <blashko.brett@gmail.com>
2017-03-15 00:21:58 -0700

    fixed merge errors

--- assignment1/src/BSplineScene.h
+++ assignment1/src/BSplineScene.h
@@ -15,13 +15,10 @@ namespace assignment1
     class BSplineScene : public atlas::tools::ModellingScene
     {
     public:
-        std::vector<assignment1::Circle> circlePoints;
-
         BSplineScene();
 
         void mousePressEvent(int button, int action, int modifiers,
             double xPos, double yPos) override;
-        void renderScene() override;
         void renderScene() override;
     private:
         BSpline bSpline;    
